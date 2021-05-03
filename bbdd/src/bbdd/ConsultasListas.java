d619f13e0f696dccf338cd0ec4ab7ec3717be921
Aitor Calderon <aitor.calderonlo@elorrieta-errekamari.com>
2021-05-03 10:16:24 +0200

    Se pasan los datos como rs

--- bbdd/src/bbdd/ConsultasListas.java
+++ bbdd/src/bbdd/ConsultasListas.java
@@ -3,7 +3,6 @@ package bbdd;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class ConsultasListas {
 
@@ -31,45 +30,33 @@ public class ConsultasListas {
 		return rs;
 	}
 
-	public ArrayList<String[]> cogerProductosAprovisionamiento() {
-		ArrayList<String[]> listaProd = new ArrayList<String[]>();
+	public ResultSet cogerProductosAprovisionamiento() {
+		ResultSet rs = null;
 		try {
 			PreparedStatement st = null;
 			st = (PreparedStatement) ((java.sql.Connection) conexionConn)
 					.prepareStatement(sentenciasBBDD.ALIMENTOORDENADO);
-			ResultSet rs = st.executeQuery();
-			while (rs.next()) {
-				String nombre = rs.getString("a.nombre");
-				String pCompra = String.valueOf(rs.getDouble("a.PCompra"));
-				String tipo = rs.getString("a.Tipo");
-				String feCad = String.valueOf(rs.getDate("a.FeCad"));
-				String[] producto = new String[] { nombre, feCad, tipo, pCompra };
-				listaProd.add(producto);
-			}
+			rs = st.executeQuery();
+			
 		} catch (SQLException sqlException) {
 			sqlException.printStackTrace();
 		}
-		return listaProd;
+		return rs;
 	}
-
-	public ArrayList<String[]> cogerListaPlatos(String NIFLocal) {
-		ArrayList<String[]> listaProd = new ArrayList<String[]>();
+	
+	public ResultSet cogerListaPlatos(String NIFLocal) {
+		ResultSet rs = null;
 		try {
 			PreparedStatement st = null;
 			st = (PreparedStatement) ((java.sql.Connection) conexionConn)
 					.prepareStatement(sentenciasBBDD.PLATOJOINCARTA);
 			st.setString(1, NIFLocal);
-			ResultSet rs = st.executeQuery();
-			while (rs.next()) {
-				String nombre = rs.getString("p.Nombre");
-				String pvp = String.valueOf(rs.getDouble("p.pvp"));
-				String[] producto = new String[] {nombre, pvp};
-				listaProd.add(producto);
-			}
+			rs = st.executeQuery();
+			
 		} catch (SQLException sqlException) {
 			sqlException.printStackTrace();
 		}
-		return listaProd;
+		return rs;
 	}
 
 }
