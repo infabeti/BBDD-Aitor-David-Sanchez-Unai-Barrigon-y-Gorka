aec80c2b2aac7c0e80528b6d73e8b46feadf2709
AitorCalderonLo <aitor.calderonlo@elorrieta-errekamari.com>
2021-05-03 17:31:39 +0200

    se elimina dependencia bbdd

---
+++ bbdd/src/bbdd/EjecutarAccion.java
@@ -0,0 +1,34 @@
+package bbdd;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class EjecutarAccion {
+
+	private java.sql.Connection connection;
+
+	public EjecutarAccion(Conexion conexion) {
+		connection = conexion.getConn();
+	}
+
+	public ResultSet consultar(PreparedStatement ps) {
+
+		ResultSet resultSet = null;
+		try {
+			resultSet = ps.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return resultSet;
+	}
+
+	public void insertar(PreparedStatement ps) {
+		try {
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
